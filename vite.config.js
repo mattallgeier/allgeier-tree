58bba4cfc816ceaa2b7e5271dd74c911e676a381
mattallgeier <mattallgeier@gmail.com>
2026-02-27 19:44:56 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Change 'allgeier-tree' to your GitHub repo name before deploying
 export default defineConfig({
   plugins: [react()],
-  base: '/allgeier-tree/',
+  base: "/",
 })
